17d29466fcf1aea62989bcc860b368458a3eb5d1
Momchil Ignatov <momchilki427@gmail.com>
2026-03-01 16:35:09 +0100

    Created Solution (ASP.NET MVC)

---
+++ Vitalis/Vitalis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vitalis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
