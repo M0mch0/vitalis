44eccef2e2d376986d933bae478fcb8a44829f25
Momchil Ignatov <momchilki427@gmail.com>
2026-03-02 15:01:56 +0100

    Added CatalogController and navbar dropdown for its actions to the layout. Added empty razorpages views for actions Index, Ingredients, Meals, Tags.

---
+++ Vitalis/Vitalis/Controllers/CatalogController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vitalis.Data;
+using Vitalis.Models;
+
+namespace Vitalis.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly VitalisDbContext dbContext;
+
+        public CatalogController(VitalisDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Meals()
+        {
+            return View();
+        }
+        public IActionResult Ingredients()
+        {
+            return View();
+        }
+        public IActionResult Tags()
+        {
+            return View();
+        }
+
+    }
+}
